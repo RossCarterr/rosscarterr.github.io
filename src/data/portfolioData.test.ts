dcc67dc14dddc17a652c2a03ed48e22a1cdc4dd2
Ross <ross29.8@hotmail.com>
2026-07-31 12:36:39 +0100

    missed tests

--- src/data/portfolioData.test.ts
+++ src/data/portfolioData.test.ts
@@ -15,7 +15,7 @@ describe("portfolio data", () => {
             cv: "https://github.com/RossCarterr/CV/raw/main/Ross'_CV.pdf",
             github: "https://github.com/RossCarterr",
             linkedin: "https://www.linkedin.com/in/ross-carterr",
-            email: "mailto:ross29.8@hotmail.com",
+            email: "mailto:ross@rosscarterr.co.uk",
         });
     });
 
