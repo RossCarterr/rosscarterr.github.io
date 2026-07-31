3f462969ea41d1e1b52e2b828354f5b9e8e0462f
Ross <ross29.8@hotmail.com>
2026-07-31 12:23:53 +0100

    Refine skills and updated links

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,5 +2,5 @@ export const portfolioLinks = {
     cv: "https://github.com/RossCarterr/CV/raw/main/Ross'_CV.pdf",
     github: "https://github.com/RossCarterr",
     linkedin: "https://www.linkedin.com/in/ross-carterr",
-    email: "mailto:ross29.8@hotmail.com",
+    email: "mailto:ross@rosscarterr.com",
 } as const;
